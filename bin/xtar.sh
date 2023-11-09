342b4b8bdb480aaadba8e3e22c2b49af0185c517
Zack Winkles <hymenaeus0@disroot.org>
2023-11-09 12:04:56 -0800

    xtar.sh: just read it

--- bin/xtar.sh
+++ bin/xtar.sh
@@ -4,7 +4,6 @@
 __XTAR_SH=1
 
 FUNCTIONS_ROOT="$HOME/etc/functions.d"
-#. "$FUNCTIONS_ROOT/colors.sh" || exit 1
 . "$FUNCTIONS_ROOT/msg.sh" || exit 1
 . "$FUNCTIONS_ROOT/debug.sh" || exit 1
 
@@ -16,121 +15,126 @@ declare -i KEEP_INPUT=0
 declare -i VERBOSE=0
 
 usage() {
-    _msg "Usage: $(basename $0) [-<1-19>] [-k] <INPUT> [INPUT2] ...\n" >&2
+	_msg "Usage: $(basename $0) [-<1-19>] [-k] <INPUT> [INPUT2] ...\n" >&2
 }
 
 cleanup() {
-  if (( completed )); then
-    if [ -n "${base:-}" ]; then
-      rm -f "$base.tar" "$base.git.tar"
-    fi
-  else
-    rm -f "$base.tar" "$base.git.tar" "$base.tar.zst" "$base.git.tar.zst"
-  fi
+	if (( completed )); then
+		if [ -n "${base:-}" ]; then
+			rm -f "$base.tar" "$base.git.tar"
+		fi
+	else
+		rm -f "$base.tar" "$base.git.tar" "$base.tar.zst" "$base.git.tar.zst"
+	fi
 }
 trap cleanup EXIT
 
 process_input() {
-    local input="$1"
-    local base="$(basename $input)"
-    local dir="$(dirname $input)"
-    local startdir="$(pwd)"
-    local finalsize=''
-    declare -gi completed=0
+	local input="$1"
+	local base="$(basename $input)"
+	local dir="$(dirname $input)"
+	local startdir="$(pwd)"
+	local finalsize=''
+	declare -gi completed=0
 
-    _msg "${bold}processing input '$cyn$base$wht'$rst...\n"
+	_msg "${bold}processing input '$cyn$base$wht'$rst...\n"
 
-    [[ "$dir" = '.' ]] || cd "$dir"
+	[[ "$dir" = '.' ]] || cd "$dir"
 
-    if [ -d "$base"/.git ]; then
-        case "$base" in
-            *.git)
-                ;;
-            *)
-                _info "renaming '$cyn$base$wht' to '$cyn$base$wht.git'\n"
-                _nofail mv -f "$base" "$base.git"
-                base="$base.git"
-                _success
-                ;;
-        esac
-    else
-        case "$base" in
-            *.git)
-                _info "renaming '$cyn$base$wht' to '$cyn${base/.git}$wht'\n"
-                _nofail mv -f "$base" "${base/.git}"
-                base="${base/.git}"
-                _success
-                ;;
-            *)
-                ;;
-        esac
-    fi
+	if [ -d "$base"/.git ]; then
+		case "$base" in
+				*.git)
+				;;
+			*)
+				_info "renaming '$cyn$base$wht' to '$cyn$base$wht.git'"
+				_nofail mv -f "$base" "$base.git"
+				base="$base.git"
+				_success
+				;;
+		esac
+	else
+		case "$base" in
+			*.git)
+				_info "renaming '$cyn$base$wht' to '$cyn${base/.git}$wht'\n"
+				_nofail mv -f "$base" "${base/.git}"
+				base="${base/.git}"
+				_success
+				;;
+			*)
+				;;
+		esac
+	fi
 
-    _info "creating tarball '$cyn$base.tar$wht'"
-    tar --sort=name \
-        --preserve-permissions \
-        --same-owner \
-        --xattrs \
-        --xattrs-include='*.*' \
-        --create \
-        --file "$base.tar" \
-        "$base" || _fatal "tar failed"
-    _success
+	_info "creating tarball '$cyn$base.tar$wht'"
+	tar --sort=name \
+	    --preserve-permissions \
+	    --same-owner \
+	    --xattrs \
+	    --xattrs-include='*.*' \
+	    --create \
+	    --file "$base.tar" \
+	    "$base" || \
+			    _fatal "tar failed"
+	_success
+
+	uncompressed_size="$(ls -lh $base.tar | awk '{print $5}')"
+
+	_info "compressing '$cyn$base.tar$wht' with '${red}zstd -$COMP_LVL$rst'"
+	zstd -$COMP_LVL \
+	     --threads=0 \
+	     --quiet \
+	     --force \
+	     --rm \
+	     "$base.tar" || \
+			     _fatal "zstd failed\n"
+	completed=1
+	_success
 
 	if ! (( KEEP_INPUT )); then
 		_info "removing '$blu$base/$wht' directory"
 		rm -rf "$base/" || _fatal "rm failed\n"
-    _success
+		_success
 	fi
 
-    _info "compressing '$cyn$base.tar$wht' with '${red}zstd -$COMP_LVL'"
-    zstd -$COMP_LVL \
-        --threads=0 \
-        --quiet \
-        --force \
-        --rm \
-        "$base.tar" || _fatal "zstd failed\n"
-    completed=1
-    _success
-
 	finalsize="$(ls -lh $base.tar.zst | awk '{print $5}')"
-  _msg " $dim[$bold$ylw☆$rst$dim]$rst final size: $mag$finalsize$wht ($cyn$base.tar.zst$wht)\n"
+	_msg " $dim[$bold$ylw☆$rst$dim]$rst uncompressed size: $mag$uncompressed_size$wht ($cyn$base.tar.zst$wht)\n"
+	_msg " $dim[$bold$grn☆$rst$dim]$rst final size: $mag$finalsize$wht ($cyn$base.tar.zst$wht)\n"
 
 	echo
 
-    [[ "$dir" = '.' ]] || cd "$startdir"
+	[[ "$dir" = '.' ]] || cd "$startdir"
 }
 
 while [ -n "${1:-}" ]; do
-    case "$1" in
-        -h|--help)
-            usage
-            exit 0
-            ;;
-        -[1-9]|-1[0-9])
-            COMP_LVL=${1/-}
-            shift 1
-            ;;
-        -k|--keep)
-            KEEP_INPUT=1
-            shift 1
-            ;;
-        -*)
-            _fatal "Invalid argument: '$1'\n"
-            ;;
-        *)
-            INPUTS+=("$1")
-            shift 1
-            ;;
-    esac
+	case "$1" in
+		-h|--help)
+			usage
+			exit 0
+			;;
+		-[1-9]|-1[0-9])
+			COMP_LVL=${1/-}
+			shift 1
+			;;
+		-k|--keep)
+			KEEP_INPUT=1
+			shift 1
+			;;
+		-*)
+			_fatal "Invalid argument: '$1'\n"
+			;;
+		*)
+			INPUTS+=("$1")
+			shift 1
+			;;
+	esac
 done
 
 if [ ${#INPUTS[@]} -eq 0 ]; then
-    _fatal "No input files given.\n"
+	_fatal "No input files given.\n"
 fi
 
 for input in ${INPUTS[@]}; do
-    process_input "$input"
+	process_input "$input"
 done
 
-# vim: ts=2 sw=2 ft=bash
+# vim: ts=2 sw=2 noet ai ft=bash
